e95c6b537dbe3d02fec15efdebb8d8ccbae770e3
Camilo Montoya :) <camilo.montoya.contractor@macmillan.com>
2024-04-27 12:52:47 -0500

    Remove email field from User create method

--- app/src/create.php
+++ app/src/create.php
@@ -7,28 +7,12 @@ require_once 'Database.php';
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     // Get the form data
     $name = $_POST['name'];
-    $email = $_POST['email'];
-    $password = $_POST['password'];
-
-    // Create a new User instance
+    
     $user = new User();
 
     // Set the user data
     $user->setName($name);
-    $user->setEmail($email);
-    $user->setPassword($password);
-
-    // Save the user to the database
-    $database = new Database(
-      "localhost",
-      "test",
-      "test",
-      "test"
-    );
-    $database->connect();
-    $database->createUser($user);
-    $database->disconnect();
-
+    
     // Redirect to the index page
     header('Location: index.php');
     exit;
@@ -46,10 +30,6 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     <form method="POST" action="create.php">
         <label for="name">Name:</label>
         <input type="text" name="name" id="name" required><br><br>
-        <label for="email">Email:</label>
-        <input type="email" name="email" id="email" required><br><br>
-        <label for="password">Password:</label>
-        <input type="password" name="password" id="password" required><br><br>
         <input type="submit" value="Create">
     </form>
 </body>
