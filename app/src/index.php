90a8eb2da0f89ddcdb8767b5928725473757a547
Camilo Montoya :) <camilo.montoya.contractor@macmillan.com>
2024-04-27 14:41:18 -0500

    chore: Remove unnecessary code for converting timestamps to local time zone in index.php

--- app/src/index.php
+++ app/src/index.php
@@ -23,8 +23,6 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
         echo "Name: {$user['name']}<br>";
         echo "created_at: {$user['created_at']}<br>";
         echo "updated_at: {$user['updated_at']}<br>";
-        $createdAt = new DateTime($user['created_at'], new DateTimeZone('UTC'));
-        $createdAt->setTimezone(new DateTimeZone('America/New_York'));
         echo "<script>const date = new Date('{$createdAt->format('Y-m-d H:i:s')}'); console.log(date);</script>";
         echo "<script>document.getElementById</script>";
     }
